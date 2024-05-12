7588991a3121ea33558abc167ff234cc5c50bbdc
Khoa Tran <khoatrandd.139@gmail.com>
2024-05-12 13:00:08 +0700

    finish func

--- Revision/func.hs
+++ Revision/func.hs
@@ -16,3 +16,34 @@ False &&& _ = False
 (|||) :: Bool -> Bool -> Bool
 True  ||| _ = True
 False ||| x = x
+
+myhead :: [a] -> a
+myhead (x:_) = x
+
+mytail :: [a] -> [a]
+mytail [] = []
+mytail (_:xs) = xs
+
+mylast :: [a] -> a
+mylast [x] = x
+mylast (_:xs) = mylast xs
+
+--
+-- THIS IS NOT HOW PATTERN MATCHING
+-- WORKS IN HASKELL
+-- myinit :: [a] -> [a]
+-- myinit (xs:_) = xs
+--
+
+myadd :: Int -> (Int -> Int)
+myadd = \x -> (\y -> x + y)
+
+myodds :: Int -> [Int]
+myodds n = map (\x -> 2*x + 1) [0..n-1]
+
+safetail :: [a] -> [a]
+safetail xs = if null xs then [] else tail xs
+
+safetail2 :: [a] -> [a]
+safetail2 xs | null xs   = []
+             | otherwise = tail xs
